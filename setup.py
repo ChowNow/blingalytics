af467a834901845afbe09e4a6091c2e7eaa4fd90
MJ Hardin <mhardinla@gmail.com>
2023-06-21 15:33:02 -0700

    use semantic version

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ open-source project. Blingalytics is released under the `MIT License`_.
 
 setup(
     name='Blingalytics',
-    version='v0.2.2chownow',
+    version='0.2.2',
     author='Jeff Schenck',
     author_email='jmschenck@gmail.com',
     url='http://github.com/jeffschenck/blingalytics',
